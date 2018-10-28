98675548a7b1a3e3233ce1482bb2b47d4601cca9
Samantha Johnston <samareimorgan@gmail.com>
2018-10-28 17:14:47 -0600

    Add ORM

--- app/controllers/bookroutes.js
+++ app/controllers/bookroutes.js
@@ -1,3 +1,19 @@
 //Create Dependencies
 var express = require("express");
 var router = express.Router();
+
+//Import model to utilize the database functions
+var book = require("../models/books");
+
+//create html route (homepage)
+router.get("/", function(req, res) {
+    book.all(function(data) {
+      var hbsObject = {
+        books: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+router.post()
